7f3d065fc268cb256d93c44052fd232dc43b8516
Faris Faikar <farisfaikar.r@gmail.com>
2024-07-04 16:14:51 +0700

    feat(model): add item and news models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,17 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class News extends Model implements HasMedia
+class Item extends Model implements HasMedia
 {
-    use HasFactory;
-    use InteractsWithMedia;
+    use HasFactory, InteractsWithMedia;
 
-    public function items()
+    public function news()
     {
-        return $this->hasMany(Item::class);
+        return $this->belongsTo(\App\Models\News::class);
     }
 }
